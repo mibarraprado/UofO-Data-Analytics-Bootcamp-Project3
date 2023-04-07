ccd0d36c814dcea197c74abb640ca646f1852222
Reginald Robinson <ddrbnsn@gmail.com>
2023-04-06 21:46:54 -0700

    still doing voting

--- page/templates/scripts/index.js
+++ page/templates/scripts/index.js
@@ -1,5 +1,5 @@
 /**
- *TODO: Figure out how to communicate with flask server from this js file. 
+ *TODO: Figure out how to communicate with flask server from this js file. https request sbaby
  *TODO: Probably has to send http request back 
  *TODO: and it will pick it up but ive yet to do such a thing in python
  *TODO: lol lets go chef gang brrrr 
@@ -11,6 +11,10 @@
 
  */
 
+const handleVote = (str)=>{
+    voteCast(str)
+    showVotingTotals()
+}
 
 const voteCast = (str) => {
     fetch("localhost:5000/chefkiss/vote", {
@@ -20,4 +24,14 @@ const voteCast = (str) => {
         },
         body: JSON.stringify(str) // Attach the JSON data as the request body
       })
+}
+
+const showVotingTotals = () => {
+    fetch("localhost:5000/chefkiss/voteTot")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        // make a chart with data
+        //add chart to html 
+    })
 }
